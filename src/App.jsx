5a2dab48abae1f41dce96711ef1bc6efcab9a042
Subhan Tariq <stariq2902@gmail.com>
2024-10-08 14:11:42 -0400

    Added Basic SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search_bar from "./components/SearchBar/search_bar";
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/"></Route>
-          <Route path="/signup"></Route>
-          <Route path="/login"></Route>
-          <Route path="/account"></Route>
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/"></Route>
+            <Route path="/signup"></Route>
+            <Route path="/login"></Route>
+            <Route path="/account"></Route>
+          </Routes>
+        </Router>
+      </div>
+      <div className="search_bar">
+        <Search_bar></Search_bar>
+      </div>
+    </>
   );
 };
 
