f6265488d307e04509cbbc90afa961c13a8546d2
Subhan Tariq <stariq2902@gmail.com>
2024-10-08 20:34:40 -0400

    Search_bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search_bar from "./components/search_bar/search_bar";
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/"></Route>
-          <Route path="/signup"></Route>
-          <Route path="/login"></Route>
-          <Route path="/account"></Route>
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/"></Route>
+            <Route path="/signup"></Route>
+            <Route path="/login"></Route>
+            <Route path="/account"></Route>
+          </Routes>
+        </Router>
+      </div>
+      <div>
+        <Search_bar></Search_bar>
+      </div>
+    </>
   );
 };
 
