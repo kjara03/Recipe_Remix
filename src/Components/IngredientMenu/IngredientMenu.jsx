c6858e03883257c0970b4fa9ffdfce6840ce9abc
Kevin Zheng <kevinzheng700@gmail.com>
2024-10-16 23:25:37 -0400

    Update background image

--- src/Components/IngredientMenu/IngredientMenu.jsx
+++ src/Components/IngredientMenu/IngredientMenu.jsx
@@ -1,35 +1,31 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./IngredientMenu.css";
 import Select from "react-select";
 
-
 const IngredientMenu = () => {
-    
-    const options = [
-        {value: 1, label: 'Chicken'},
-        {value: 2, label: 'Fish'},
-        {value: 3, label: 'Apple'},
-        {value: 4, label: 'Cabbage'},
-        {value: 5, label: 'Carrot'},
-    ];
+  const options = [
+    { value: 1, label: "Chicken" },
+    { value: 2, label: "Fish" },
+    { value: 3, label: "Apple" },
+    { value: 4, label: "Cabbage" },
+    { value: 5, label: "Carrot" },
+  ];
 
-    const [selectedOptions, setSelectedOptions] = useState([])
-    const handleChange = (selectedOptions) => {
-        setSelectedOptions(selectedOptions);
-    };
-    
-    
-    return (
-        <div>
-            <Select
-                options={options}
-                value={selectedOptions}
-                onChange={handleChange}
-                isMulti={true}
-            />
-        </div>
-    );
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const handleChange = (selectedOptions) => {
+    setSelectedOptions(selectedOptions);
+  };
 
+  return (
+    <div>
+      <Select
+        options={options}
+        value={selectedOptions}
+        onChange={handleChange}
+        isMulti={true}
+      />
+    </div>
+  );
 };
 
 export default IngredientMenu;
