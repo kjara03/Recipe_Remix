5ed617aa8d480e70a6a165c6c72462d9aabcb91f
saintquartz <96217500+saintquartz@users.noreply.github.com>
2024-11-19 21:18:54 -0500

    Update SignupForm.jsx

--- client/src/components/SignupForm/SignupForm.jsx
+++ client/src/components/SignupForm/SignupForm.jsx
@@ -24,9 +24,8 @@ const SignupForm = () => {
 
   // Function to make sure the email is valid
   function validateEmail(email) {
-    return String(email).toLowerCase().match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+    var emailTest = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailTest.test(email);
   }
 
   // Function to ensure password is strong
